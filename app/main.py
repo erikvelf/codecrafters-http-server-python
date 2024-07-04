809f44cfa577f54eb5a51bf38c685c02e1de8cef
Erik Velf <erik@ew>
2024-07-04 15:12:31 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -20,10 +20,21 @@ def main():
         url = request.decode().split(' ') 
 
         target_request = url[1]
- 
+
+
+
+        
+        
+
         if target_request == '/':
             print("target_request is / ", target_request)
             connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        elif target_request.find('echo') != -1:
+            echo = url[1].split("/")[-1]
+            print("echo:", echo)
+
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}"
+            connection.sendall((response.encode()))
         else:
             print("target request wrong", target_request)
             connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
